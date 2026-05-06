2873ff20c8710b51cfe7efab5dea249239963bdb
Kimseungmin (Abel) Yu <dbrla0316@gmail.com>
2026-05-06 14:53:07 -0400

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # 이게 바이너리서치라고?
+        # 그냥 가능한 value들을 array로 나열하고 다 해보는 대신에 binary search로 해보기.
+        # ***Value들간의 크기 상관관계가 순서대로 있기 때문에 가능한 듯?***
+
+        l, r = 1, max(piles)
+        res = r
+
+        while l <= r:
+            k = int((l + r) / 2)
+            # verify k
+            hCurr = 0
+            for pile in piles:
+                hCurr += math.ceil(pile / k)
+            if hCurr <= h: # k equal or larger
+                res = k
+                r = k - 1
+            elif hCurr > h: #k too small
+                l = k + 1
+                k += 1 # !여기가 포인트! minimum k를 구하는 거니까 작은채로 끝날때 1 올려줘야함.
+        
+        return k
