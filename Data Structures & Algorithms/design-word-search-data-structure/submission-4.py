6dfc2fea9d8fcf04c0e8f50251467f9d30e9b0b2
Kimseungmin (Abel) Yu <dbrla0316@gmail.com>
2026-07-05 15:54:30 -0600

    Add: design-word-search-data-structure - submission-4

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-4.py
@@ -0,0 +1,39 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isLastChar = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.isLastChar = True
+
+    def search(self, word: str) -> bool:
+
+        def searchHelper(j, cur):
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == '.':
+                    for child in cur.children.values():
+                        if searchHelper(i + 1, child): # 순서 바뀌기
+                            return True
+                    return False
+                
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            
+            return cur.isLastChar
+        
+        return searchHelper(0, self.root)
+                
+    
