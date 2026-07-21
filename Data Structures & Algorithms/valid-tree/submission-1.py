5e5eae82814a6503bc08845d897e429cfb63a9f0
Kimseungmin (Abel) Yu <dbrla0316@gmail.com>
2026-07-20 19:28:45 -0600

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+        
+        connected = set()
+
+        def dfs(node):
+            if node in connected:
+                return
+            
+            connected.add(node)
+            
+            for edge in edges:
+                if edge[0] == node:
+                    dfs(edge[1])
+                elif edge[1] == node:
+                    dfs(edge[0])
+        
+        dfs(0)
+        
+        if len(connected) != n:
+            return False
+        
+        return True
