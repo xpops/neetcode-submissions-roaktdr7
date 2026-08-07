c40ced551f4c7536b5e4d01aa25694b09d6b0a1a
Kimseungmin (Abel) Yu <dbrla0316@gmail.com>
2026-08-07 14:24:32 -0400

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.py
@@ -0,0 +1,19 @@
+class Solution: # 1. Sorting: O(nlogn)
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        res = []
+        
+        # sort by start
+        intervals.sort(key=lambda x: x[0])
+
+        new = intervals[0]
+        for i in range(1, len(intervals)):
+            cur = intervals[i]
+            if cur[0] > new[1]: # no overlap
+                res.append(new)
+                new = cur
+            else: # overlap
+                new = [new[0], max(cur[1], new[1])]
+        
+        res.append(new) # 마지막에 남은거 넣어주기
+    
+        return res
